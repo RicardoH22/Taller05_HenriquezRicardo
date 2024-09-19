1fe188ffe90b684f80d580919020a9d4c5a4d26b
rhenriqu <rhenriqu4@cuc.edu.co>
2024-09-19 12:02:27 -0500

    m1

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+import milogo from "./assets/img/pngwing.com.png" 
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <img src={milogo} alt="logo de react" />
+      <h1>El coche mas emblematico del mundo </h1>
+      <p>Tenia mucha tecnologia y estaba muy avanzado para su epoca </p> 
+      </div>
+
+    </>
+  )
+}
+
+export default App
